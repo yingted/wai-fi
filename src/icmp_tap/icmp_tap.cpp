3e013b73e1afe536e7c8dbfbebc5fba7b87da09e
Ted Ying <yingted@gmail.com>
2015-06-12 12:24:56 -0400

    read echos

--- src/icmp_tap/icmp_tap.cpp
+++ src/icmp_tap/icmp_tap.cpp
@@ -13,15 +13,15 @@ using namespace std;
 #include <linux/if.h>
 #include <linux/if_tun.h>
 #include <linux/icmp.h>
-#include <linux/in.h>
+#include <linux/ip.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
 
 #include "tuntap.h"
-#include "icmp_tap_tx.h"
 
 int main(int argc, char *argv[]) {
 	char dev[IFNAMSIZ] = "icmp0";
-	char buf[1500];
+	char buf[64 * 1024]; // max IPv4 packet size
 	int tap_fd, raw_fd, fd_max;
 	fd_set fds;
 
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	struct icmp_filter filt;
-	filt.data = ~(1U << 0U);
+	filt.data = ~(1U << ICMP_ECHO);
 	if (setsockopt(raw_fd, IPPROTO_RAW, ICMP_FILTER, &filt, sizeof(filt)) < 0) {
 		perror("setsockopt");
 	}
@@ -56,11 +56,31 @@ int main(int argc, char *argv[]) {
 		// Subscribe any active connections to the stream.
 		// Assume that writing to tap is non-blocking.
 		if (FD_ISSET(raw_fd, &fds)) {
-			ssize_t len = recv(raw_fd, buf, sizeof(...), MSG_DONTWAIT | MSG_WAITALL);
+			struct iphdr *ip = (struct iphdr *)buf;
+			ssize_t len = recv(raw_fd, buf, sizeof(buf), MSG_DONTWAIT);
+			if (len < 0) {
+				perror("recv ip");
+				return -1;
+			}
+			ssize_t ip_hlen = ip->ihl * 4;
+			struct icmphdr *icmp = (struct icmphdr *)(((char *)ip) + ip_hlen);
+			unsigned short tot_len = ntohs(ip->tot_len);
+			if (len != tot_len) {
+				printf("Expected tot_len=%d, got tot_len=%d\n", len, tot_len);
+			} else {
+				char *data = ((char *)icmp) + sizeof(*icmp);
+				size_t data_len = buf + len - data;
+				printf("got %d bytes, %d\n", data_len, icmp->type);
+
+				if (icmp->type == ICMP_ECHO) {
+					printf("id=%d seq=%d saddr=%u\n", ntohs(icmp->un.echo.id), ntohs(icmp->un.echo.sequence), ip->saddr);
+					// XXX enqueue these
+				}
+			}
 		}
 		// Step 2: Collect data from the tap fd and update the outgoing queues.
 		if (FD_ISSET(tap_fd, &fds)) {
-			l = read(tap_fd, buf, sizeof(buf));
+			//l = read(tap_fd, buf, sizeof(buf));
 		}
 		// Step 3: Write out the data to each connection.
 		// Also time out some connections.
