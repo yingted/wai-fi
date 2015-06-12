facbb175f0ec57384cb2db4acb1184819e166ddd
Ted Ying <yingted@gmail.com>
2015-06-12 15:16:39 -0400

    finish icmp_tap

--- src/icmp_tap/icmp_tap.cpp
+++ src/icmp_tap/icmp_tap.cpp
@@ -18,6 +18,8 @@ using namespace std;
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+#include <boost/circular_buffer.hpp>
 
 #include "tuntap.h"
 
@@ -27,15 +29,24 @@ struct icmp_reply {
 	struct timespec time;
 };
 
+struct connection {
+	boost::circular_buffer<char>::size_type pos;
+	set<icmp_reply> replies;
+};
+
 static bool lt_seq(const icmp_reply &a, const icmp_reply &b) {
 	return a.seq < b.seq;
 }
 
-static bool lt_time(const icmp_reply &a, const icmp_reply &b) {
-	if (a.time.tv_sec != b.time.tv_sec) {
-		return a.time.tv_sec < b.time.tv_sec;
+static bool operator<(const struct timespec &a, const struct timespec &b) {
+	if (a.tv_sec != b.tv_sec) {
+		return a.tv_sec < b.tv_sec;
 	}
-	return a.time.tv_nsec < b.time.tv_nsec;
+	return a.tv_nsec < b.tv_nsec;
+}
+
+static bool lt_time(const icmp_reply &a, const icmp_reply &b) {
+	return a.time < b.time;
 }
 
 bool operator<(const icmp_reply &a, const icmp_reply &b) {
@@ -46,9 +57,11 @@ int main(int argc, char *argv[]) {
 	char dev[IFNAMSIZ] = "icmp0";
 	char buf[64 * 1024]; // max IPv4 packet size
 	int tap_fd, raw_fd, fd_max;
+	boost::circular_buffer<char> outq(1024 * 1024);
+	boost::circular_buffer<char>::size_type tot_recv = 0; // represents outq.end()
 	fd_set fds;
 
-	map<__be32, set<icmp_reply> > replies;
+	map<__be32, connection> conns;
 
 	if ((tap_fd = tun_alloc(dev, IFF_TAP)) < 0) {
 		perror("tun_alloc");
@@ -92,42 +105,99 @@ int main(int argc, char *argv[]) {
 			ssize_t len = recv(raw_fd, buf, sizeof(buf), MSG_DONTWAIT);
 			if (len < 0) {
 				perror("recv ip");
-				return -1;
-			}
-			ssize_t ip_hlen = ip->ihl * 4;
-			struct icmphdr *icmp = (struct icmphdr *)(((char *)ip) + ip_hlen);
-			unsigned short tot_len = ntohs(ip->tot_len);
-			if (len != tot_len) {
-				printf("Expected tot_len=%d, got tot_len=%d\n", len, tot_len);
 			} else {
-				char *data = ((char *)icmp) + sizeof(*icmp);
-				ssize_t data_len = buf + len - data;
+				ssize_t ip_hlen = ip->ihl * 4;
+				struct icmphdr *icmp = (struct icmphdr *)(((char *)ip) + ip_hlen);
+				unsigned short tot_len = ntohs(ip->tot_len);
+				if (len != tot_len) {
+					printf("Expected tot_len=%d, got tot_len=%d\n", len, tot_len);
+				} else {
+					char *data = ((char *)icmp) + sizeof(*icmp);
+					ssize_t data_len = buf + len - data;
 
-				if (icmp->type == ICMP_ECHO) {
-					icmp_reply reply;
-					reply.id = ntohs(icmp->un.echo.id);
-					reply.seq = ntohs(icmp->un.echo.sequence);
-					reply.addr = ip->saddr;
-					clock_gettime(CLOCK_MONOTONIC, &reply.time);
-					printf("id=%d seq=%d saddr=%u\n", reply.id, reply.seq, reply.addr);
-					replies[reply.addr].insert(reply);
+					if (icmp->type == ICMP_ECHO) {
+						icmp_reply reply;
+						reply.id = ntohs(icmp->un.echo.id);
+						reply.seq = ntohs(icmp->un.echo.sequence);
+						reply.addr = ip->saddr;
+						clock_gettime(CLOCK_MONOTONIC, &reply.time);
+						printf("id=%d seq=%d saddr=%s\n", reply.id, reply.seq, inet_ntoa(*(in_addr *)&reply.addr));
+						bool new_conn = !conns.count(reply.addr);
+						connection &conn = conns[reply.addr];
+						if (new_conn) {
+							printf("new connection to %s\n", inet_ntoa(*(in_addr *)&reply.addr));
+							conn.pos = tot_recv;
+						}
+						conn.replies.insert(reply);
 
-					ssize_t written = write(tap_fd, data, data_len);
-					if (written < 0) {
-						perror("write");
-					} else if (written != data_len) {
-						printf("wrote %d instead of %d\n", written, data_len);
+						ssize_t written = write(tap_fd, data, data_len);
+						if (written < 0) {
+							perror("write");
+						} else if (written != data_len) {
+							printf("wrote %d instead of %d\n", written, data_len);
+						}
 					}
 				}
 			}
 		}
-		// Step 2: Collect data from the tap fd and update the outgoing queues.
+		// Step 2: Collect data from the tap fd and update the outgoing queue.
 		if (FD_ISSET(tap_fd, &fds)) {
-			//l = read(tap_fd, buf, sizeof(buf));
+			ssize_t len = recv(raw_fd, buf, sizeof(buf), MSG_DONTWAIT);
+			if (len < 0) {
+				perror("recv tap");
+			} else {
+				copy(buf, buf + len, back_inserter(outq));
+				tot_recv += len;
+			}
 		}
 		// Step 3: Write out the data to each connection.
 		// Also time out some connections.
-		// Split via:
-		// getsockopt(socket, SOL_SOCKET, SO_MAX_MSG_SIZE, (int *)&optval, &optlen);
+		// Split by:
+		// getsockopt(raw_fd, SOL_SOCKET, SO_MAX_MSG_SIZE, (int *)&optval, &optlen);
+		struct timespec min_time;
+		clock_gettime(CLOCK_MONOTONIC, &min_time);
+		min_time.tv_sec -= 30; // 30 seconds timeout
+		for (auto &it : conns) {
+			connection &conn = it.second;
+			ssize_t to_write = tot_recv - conn.pos;
+			ssize_t mtu = 1400 - 1; // for header
+			int packets = (to_write + mtu - 1) / mtu;
+			unsigned char queued = max<unsigned char>(0, min<unsigned long>(UCHAR_MAX, packets - conn.replies.size()));
+			for (auto it = conn.replies.begin(); it != conn.replies.end(); conn.replies.erase(it++)) {
+				const icmp_reply &reply = *it;
+				printf("id=%d seq=%d saddr=%s\n", reply.id, reply.seq, inet_ntoa(*(in_addr *)&reply.addr));
+				assert(conn.pos < tot_recv);
+				ssize_t send_len = max<long>(mtu, tot_recv - conn.pos);
+				assert(send_len <= to_write);
+
+				buf[0] = queued;
+				copy(outq.end() - to_write, outq.end() - (to_write - send_len), buf + 1);
+				to_write -= send_len;
+				send_len += 1;
+
+				struct sockaddr_in dst = {
+					.sin_family = AF_INET,
+					.sin_port = 0,
+					.sin_addr = {
+						.s_addr = reply.addr,
+					},
+				};
+				ssize_t sent = sendto(raw_fd, buf, send_len, MSG_DONTWAIT, (struct sockaddr *)&dst, sizeof(dst));
+				if (sent < 0) {
+					perror("sendto");
+				} else if (send_len != sent) {
+					printf("sent %u instead of %u\n", sent, send_len);
+				}
+			}
+
+			for (auto it = conn.replies.begin(); it != conn.replies.end();) {
+				const icmp_reply &reply = *it;
+				auto reply_it = it++;
+				if (reply.time < min_time) {
+					printf("timing out id=%d seq=%d saddr=%s\n", reply.id, reply.seq, inet_ntoa(*(in_addr *)&reply.addr));
+					conn.replies.erase(reply_it);
+				}
+			}
+		}
 	}
 }
