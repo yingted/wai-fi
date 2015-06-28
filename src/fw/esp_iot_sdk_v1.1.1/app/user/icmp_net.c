ceb503c9dfeda79bdc94cd03b8206981bbd78b04
Ted Ying <yingted@gmail.com>
2015-06-28 10:02:33 -0400

    weaken assertion

--- src/fw/esp_iot_sdk_v1.1.1/app/user/icmp_net.c
+++ src/fw/esp_iot_sdk_v1.1.1/app/user/icmp_net.c
@@ -244,7 +244,7 @@ static void process_queued_pbufs() {
 #ifdef DEBUG_ESP
         user_dprintf("netif->input %d", p->tot_len);
 #endif
-        assert(p->ref == 1);
+        assert(p->ref >= 1);
 
         err_t rc = config->netif->input(p, config->netif);
         if (rc != ERR_OK) {
