d043a2b26021f49f213a3eb362d4169343bf552d
Ted Ying <yingted@gmail.com>
2015-07-01 12:21:44 -0400

    refactor overlay

--- src/imager/gen_misc.py
+++ src/imager/gen_misc.py
@@ -10,29 +10,8 @@ import tempfile
 app_dir = '../fw/esp_iot_sdk_v1.1.1/app'
 user_dir = os.path.join(app_dir, user_dir)
 
-@contextlib.contextmanager
-def overlay_applied(overlay_dir, user_dir):
-	backups = {}
-	backup_dir = tempfile.mkdtemp(dir=user_dir)
-	for name in overlay.overlay_file_names:
-		displaced_path = os.path.join(user_dir, name)
-		if os.path.exists(displaced_path):
-			backup_path = os.path.join(backup_dir, name)
-			shutil.copyfile(displaced_path, backup_path)
-			backups[displaced_path] = backup_path
-	for name in overlay.overlay_file_names:
-		displaced_path = os.path.join(user_dir, name)
-		overlay_path = os.path.join(overlay_dir, name)
-		shutil.copyfile(overlay_path, displaced_path)
-	try:
-		yield
-	finally:
-		for displaced_path, backup_path in backups.iteritems():
-			os.rename(backup_path, displaced_path)
-		os.rmdir(backup_dir)
-
 def call(overlay_dir):
-	with lockfile.LockFile(app_dir), overlay_applied(overlay_dir, user_dir):
+	with lockfile.LockFile(app_dir), overlay.overlay_applied(overlay_dir, user_dir):
 		gen_misc_path = os.path.join(app_dir, 'gen_misc.sh')
 		p = subprocess.Popen(('bash', 'gen_misc.sh'), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 		stdout, stderr = p.communicate()
--- src/imager/overlay.py
+++ src/imager/overlay.py
@@ -5,18 +5,45 @@ import shutil
 import sqlalchemy
 import models
 import gen_misc
+import subprocess
 
 overlay_file_names = 'default_private_key', 'default_certificate', 'default_private_key.c', 'default_certificate.c'
+
+@contextlib.contextmanager
+def overlay_applied(overlay_dir, user_dir):
+	backups = {}
+	backup_dir = tempfile.mkdtemp(dir=user_dir)
+	for name in overlay_file_names:
+		displaced_path = os.path.join(user_dir, name)
+		if os.path.exists(displaced_path):
+			backup_path = os.path.join(backup_dir, name)
+			shutil.copyfile(displaced_path, backup_path)
+			backups[displaced_path] = backup_path
+	for name in overlay_file_names:
+		displaced_path = os.path.join(user_dir, name)
+		overlay_path = os.path.join(overlay_dir, name)
+		shutil.copyfile(overlay_path, displaced_path)
+	subprocess.check_call(('make', '-C', user_dir) + overlay_file_names)
+	try:
+		yield
+	else:
+		for name in overlay_file_names:
+			displaced_path = os.path.join(user_dir, name)
+			overlay_path = os.path.join(overlay_dir, name)
+			shutil.copyfile(displaced_path, overlay_path)
+	finally:
+		for displaced_path, backup_path in backups.iteritems():
+			os.rename(backup_path, displaced_path)
+		os.rmdir(backup_dir)
 
 @contextlib.contextmanager
 def get_overlay_dir(mac_str, port):
 	with ...open db transaction:
 		overlay_dir = tempfile.mkdtemp(prefix='%s.' % macstr, dir='/tmp')
 		if ...INSERT overlay_dir ON CONFLICT IGNORE:
-			...populate overlay_dir
 			try:
 				yield overlay_dir
-			except Exception:
+			except:
 				shutil.rmtree(overlay_dir)
 				raise # hopefully rmtree doesn't throw
 			else:
