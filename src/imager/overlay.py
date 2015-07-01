77f854be8c4f0e48ce73f24c681e4b3373299dd8
Ted Ying <yingted@gmail.com>
2015-07-01 14:13:36 -0400

    clean up tmpdir

--- src/imager/overlay.py
+++ src/imager/overlay.py
@@ -43,24 +43,24 @@ overlay_file_names = 'default_private_key', 'default_certificate', 'default_priv
 def overlay_applied(overlay_dir, user_dir):
 	backups = {}
 	backup_dir = tempfile.mkdtemp(dir=user_dir)
-	for name in overlay_file_names:
-		displaced_path = os.path.join(user_dir, name)
-		if os.path.exists(displaced_path):
-			backup_path = os.path.join(backup_dir, name)
-			shutil.copyfile(displaced_path, backup_path)
-			backups[displaced_path] = backup_path
-	for name in overlay_file_names:
-		displaced_path = os.path.join(user_dir, name)
-		overlay_path = os.path.join(overlay_dir, name)
-		if os.path.exists(overlay_path):
-			shutil.copyfile(overlay_path, displaced_path)
-	subprocess.check_call(('make', '-C', user_dir) + overlay_file_names)
 	try:
+		for name in overlay_file_names:
+			displaced_path = os.path.join(user_dir, name)
+			if os.path.exists(displaced_path):
+				backup_path = os.path.join(backup_dir, name)
+				shutil.copyfile(displaced_path, backup_path)
+				backups[displaced_path] = backup_path
+		for name in overlay_file_names:
+			displaced_path = os.path.join(user_dir, name)
+			overlay_path = os.path.join(overlay_dir, name)
+			if os.path.exists(overlay_path):
+				shutil.copyfile(overlay_path, displaced_path)
+		subprocess.check_call(('make', '-C', user_dir) + overlay_file_names)
 		yield
 	finally:
 		for displaced_path, backup_path in backups.iteritems():
 			os.rename(backup_path, displaced_path)
-		os.rmdir(backup_dir)
+		shutil.rmtree(backup_dir)
 	for name in overlay_file_names:
 		displaced_path = os.path.join(user_dir, name)
 		overlay_path = os.path.join(overlay_dir, name)
