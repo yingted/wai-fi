972b53a33df2a7cc769fa90474e99eeaadb3d137
Ted Ying <yingted@gmail.com>
2015-08-01 22:20:55 -0400

    keep secrets in postgres

--- src/config.py
+++ src/config.py
@@ -3,5 +3,13 @@ import sqlalchemy.orm
 sdk_dir = 'fw/esp_iot_sdk_v1.1.1'
 data_dir = 'data/pki'
 
-sql_engine = sqlalchemy.create_engine('sqlite:///data/data.db')
+def _get_sql_uri():
+	import os.path
+	import re
+	with open(os.path.expanduser('~/.pgpass')) as f:
+		for line in f:
+			host, port, database, user, password = line.rstrip('\n').split(':', 5)
+			if database == 'wai-fi':
+				return 'postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s' % locals()
+sql_engine = sqlalchemy.create_engine(_get_sql_uri())
 sql_Session = sqlalchemy.orm.sessionmaker(bind=sql_engine)
