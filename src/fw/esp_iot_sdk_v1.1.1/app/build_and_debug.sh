56ee3945909d05324468014c591edf1c75719547
Ted Ying <yingted@gmail.com>
2015-08-17 13:40:27 -0400

    add build config

--- src/fw/esp_iot_sdk_v1.1.1/app/build_and_debug.sh
+++ src/fw/esp_iot_sdk_v1.1.1/app/build_and_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-GDB_STUB_STARTUP=1 bash gen_misc.sh </dev/null
+GDB_STUB_STARTUP=1 bash gen_misc.sh < build_config.txt
 esptool.py -b 921600 write_flash -ff 80m 0x00000 ../bin/eagle.flash.bin 0x40000 ../bin/eagle.irom0text.bin
 stty -F /dev/ttyUSB0 115200
 sleep 1
--- src/fw/esp_iot_sdk_v1.1.1/app/build_and_run.sh
+++ src/fw/esp_iot_sdk_v1.1.1/app/build_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-GDB_STUB_STARTUP=0 bash gen_misc.sh </dev/null
+GDB_STUB_STARTUP=0 bash gen_misc.sh < build_config.txt
 esptool.py -b 921600 write_flash -ff 80m 0x00000 ../bin/eagle.flash.bin 0x40000 ../bin/eagle.irom0text.bin
 TERM=linux exec screen /dev/ttyUSB0 115200
