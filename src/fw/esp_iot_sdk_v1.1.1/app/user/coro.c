ba479716c0e29da2461b47fe8c3b7194b45c2a7c
Ted Ying <yingted@gmail.com>
2015-08-23 12:57:06 -0400

    make coro_start_impl directly use callx0

--- src/fw/esp_iot_sdk_v1.1.1/app/user/coro.c
+++ src/fw/esp_iot_sdk_v1.1.1/app/user/coro.c
@@ -6,7 +6,7 @@
 
 __attribute__((returns_twice))
 ICACHE_FLASH_ATTR
-void coro_start_impl(struct coro_control *CORO_VOLATILE coro, size_t stacksize, register void(*func)(void *), register void *arg) {
+void coro_start_impl(struct coro_control *CORO_VOLATILE coro, size_t stacksize, void(*func)(void *), void *arg) {
     assert(coro->state == CORO_DEAD);
     assert(!coro->event); // initialized to false
     user_dprintf("");
@@ -17,8 +17,11 @@ void coro_start_impl(struct coro_control *CORO_VOLATILE coro, size_t stacksize, 
         register void *stack_top = coro->stack + stacksize;
         // Tell GCC we need to read from sp so it doesn't overlap it with stack_top
         __asm__ __volatile__("mov %[sp], a1":[sp] "=r"(sp));
-        __asm__ __volatile__("mov a1, %[stack_top]"::[stack_top] "r"(stack_top));
-        (*func)(arg);
+        __asm__ __volatile__("\
+            mov a1, %[stack_top]\n\
+            mov a2, %[arg]\n\
+            callx0 %[func]\n\
+        "::[stack_top] "r"(stack_top), [func] "r"(func), [arg] "r"(arg):"a2", "memory");
         __asm__ __volatile__("mov a1, %[sp]"::[sp] "r"(sp));
 
         assert(coro->state == CORO_RESUME);
