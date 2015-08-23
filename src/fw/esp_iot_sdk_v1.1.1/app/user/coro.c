325d91ab3844d79f873512c3cc567cb721b6a1b1
Ted Ying <yingted@gmail.com>
2015-08-22 22:27:30 -0400

    fix using stack after relocating it

--- src/fw/esp_iot_sdk_v1.1.1/app/user/coro.c
+++ src/fw/esp_iot_sdk_v1.1.1/app/user/coro.c
@@ -9,28 +9,23 @@ ICACHE_FLASH_ATTR
 void coro_start_impl(struct coro_control *CORO_VOLATILE coro, size_t stacksize, void(*func)(void *), void *arg) {
     assert(coro->state == CORO_DEAD);
     assert(!coro->event); // initialized to false
+    user_dprintf("");
     static void *volatile sp;
     if (!setjmp(coro->main)) {
         CORO_GOTO(coro, RESUME);
 
         register void *stack_top = coro->stack + stacksize;
         // Tell GCC we need to read from sp so it doesn't overlap it with stack_top
-        __asm__ __volatile__("\
-            mov %[sp], a1\n\
-            mov a1, %[stack_top]\n\
-        ":[sp] "+r"(sp):[stack_top] "r"(stack_top));
+        __asm__ __volatile__("mov %[sp], a1":[sp] "=r"(sp));
+        __asm__ __volatile__("mov a1, %[stack_top]"::[stack_top] "r"(stack_top));
         (*func)(arg);
-        __asm__ __volatile__("\
-            mov a1, %[sp]\n\
-        "::[sp] "r"(sp));
+        __asm__ __volatile__("mov a1, %[sp]"::[sp] "r"(sp));
 
         assert(coro->state == CORO_RESUME);
         assert(!coro->event);
         CORO_GOTO(coro, DEAD);
     } else {
-        __asm__ __volatile__("\
-            mov a1, %[sp]\n\
-        "::[sp] "r"(sp));
+        __asm__ __volatile__("mov a1, %[sp]"::[sp] "r"(sp));
 
         assert(coro->state == CORO_YIELD);
         assert(coro->event);
@@ -42,6 +37,7 @@ void coro_resume_impl(struct coro_control *coro, size_t what) {
     assert(coro->event);
     assert(what);
     assert((what & -what) == what);
+    user_dprintf("event=%p what=%p", coro->event, what);
     if (!(what & coro->event)) {
         return;
     }
@@ -57,6 +53,7 @@ ICACHE_FLASH_ATTR
 void coro_yield_impl(struct coro_control *coro, size_t mask) {
     assert(mask);
     coro->event = mask;
+    user_dprintf("mask=%p", mask);
 
     if (!setjmp(coro->worker)) {
         CORO_GOTO(coro, YIELD);
