949a405d068ede2891664447d51d76e440af5c27
Ted Ying <yingted@gmail.com>
2015-08-27 00:32:46 -0400

    rename to my_tcp_pcb

--- src/fw/esp_iot_sdk_v1.1.1/app/include/user_config.h
+++ src/fw/esp_iot_sdk_v1.1.1/app/include/user_config.h
@@ -34,20 +34,22 @@
 
 // Okay, this is just messed up.
 #include <lwip/tcp.h>
-struct user_tcp_pcb {
+struct my_tcp_pcb {
     IP_PCB; // IPv4 takes up [0, 11)
     char pad_12_[12 - 11];
     struct user_tcp_pcb *next; // [12, 16)
     char pad_16_[20 - 16];
     u8_t prio; // [20, 21)
-    void *callback_arg; // [20, 24)
-    char pad_24_[60 - 24];
+    char pad_21_[24 - 21];
+    void *callback_arg; // [24, 28)
+    char pad_24_[60 - 28];
     u16_t mss; // [60, 62)
     char pad_62_[148 - 62];
     tcp_err_fn errf; // [148, 152)
     u32_t keep_idle; // [152, 156)
     u32_t keep_intvl; // [156, 160)
     u32_t keep_cnt; // [160, 164)
+    char pad_164_[170 - 164];
 };
 
 #include <ssl/ssl_ssl.h>
