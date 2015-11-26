709562d67356feb25717938610f68338595daef7
Ted Ying <yingted@gmail.com>
2015-11-26 14:30:37 -0800

    Revert "Print stack info"
    This reverts commit b99da2a01f72d677f9aa333efec81bf1e1ee767e.

--- src/icmp_tap/icmp_tap.cpp
+++ src/icmp_tap/icmp_tap.cpp
@@ -2,7 +2,6 @@
 #include <cstring>
 #include "icmp_net.h"
 #include "tap.h"
-#include <boost/exception/get_error_info.hpp>
 
 int main(int argc, char *argv[]) {
 	char dev[IFNAMSIZ + 1] = "icmp0";
@@ -12,13 +11,6 @@ int main(int argc, char *argv[]) {
 	}
 	const ssize_t mtu = 1400 - 4; // for header
 
-	try {
-		icmp_net net(dev, mtu);
-		net.run();
-	} catch (const std::exception &e) {
-		std::string const *stack(boost::get_error_info<boost::error_info<struct tag_stack_str, std::string> >(e));
-		if (stack) {
-			std::cerr << stack << std::endl;
-		}
-	}
+	icmp_net net(dev, mtu);
+	net.run();
 }
